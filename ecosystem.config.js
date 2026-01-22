2b0d4e8a33295f6016a8915f1e8b80b370af6fea
krishna <ramakrishna.peddhada@gmail.com>
2026-01-22 11:23:18 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,1 +1,17 @@
-
+module.exports = {
+  apps: [
+    {
+      name: "ott-backend",
+      script: "dist/main.js",
+      instances: 1,
+      exec_mode: "fork",
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "500M",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000
+      }
+    }
+  ]
+};
